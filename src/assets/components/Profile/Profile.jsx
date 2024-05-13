f915325356c393147928f5dc3174e2896d8f7f58
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-05-13 22:05:07 +0300

    first task

---
+++ src/assets/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <>
+      <div>
+        <div>
+          <img src={avatar} alt="User avatar" />
+          <p>{username}</p>
+          <p>{tag}</p>
+          <p>{location}</p>
+        </div>
+
+        <ul>
+          <li>
+            <span>Followers</span>
+            <span>{followers}</span>
+          </li>
+          <li>
+            <span>Views</span>
+            <span>{views}</span>
+          </li>
+          <li>
+            <span>Likes</span>
+            <span>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
