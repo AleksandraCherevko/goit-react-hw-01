e2f85ac904a52991d9c0422c78b15de6e5719edf
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-05-15 15:43:48 +0300

    upd

--- src/assets/components/TransactionHistory/TransactionHistory.jsx
+++ src/assets/components/TransactionHistory/TransactionHistory.jsx
@@ -1,24 +1,28 @@
+import css from "./TransactionHistory.css";
+
 const TransactionHistory = ({ items }) => {
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <tr key={item.id}>
-              <td>{item.type}</td>
-              <td>{item.amount}</td>
-              <td>{item.currency}</td>
+      <div className={css.table}>
+        <table className={css}>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Currency</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.type}</td>
+                <td>{item.amount}</td>
+                <td>{item.currency}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
